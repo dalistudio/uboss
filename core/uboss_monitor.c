a86af5645c78f4b75495e78b5df2471fa906a0e9
wangdali <dalistudio@qq.com>
2016-05-04 00:42:07 +0800

    修改监视器的注释

--- core/uboss_monitor.c
+++ core/uboss_monitor.c
@@ -35,7 +35,7 @@ void
 uboss_monitor_trigger(struct uboss_monitor *sm, uint32_t source, uint32_t destination) {
 	sm->source = source; // 来源地址
 	sm->destination = destination; // 目的地址
-	ATOM_INC(&sm->version); // 原子加版本号
+	ATOM_INC(&sm->version); // 版本号加一
 }
 
 // 检查监视器
@@ -43,7 +43,7 @@ void
 uboss_monitor_check(struct uboss_monitor *sm) {
 	if (sm->version == sm->check_version) { // 比较版本和检查版本的值
 		if (sm->destination) { // 目的地址
-			uboss_context_endless(sm->destination); // 添加调用数
+			uboss_context_endless(sm->destination); // 结束目标服务
 			uboss_error(NULL, "A message from [ :%08x ] to [ :%08x ] maybe in an endless loop (version = %d)", sm->source , sm->destination, sm->version);
 		}
 	} else {
